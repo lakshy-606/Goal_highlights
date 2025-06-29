07580bc57db58d24be80d5cbd5ff6ffd592aff33
lakshy-606 <135644189+lakshy-606@users.noreply.github.com>
2025-06-30 00:38:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,11 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # AWS Configuration
-AWS_ACCESS_KEY_ID = 'AKIA5GCWT4SCKQT4PPO2'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME', 'football-highlights-bucket-02') #football-highlights-bucket-01
-#PAT = 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
+
 # Video Processing Configuration
 HIGHLIGHT_DURATION = 20  # Total duration in seconds
 PRE_GOAL_DURATION = 10   # Seconds before goal
